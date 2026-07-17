165fffea6ca64c1a4a770ae0782d0cfd0eb0549e
taoyunan <taoyunan@users.noreply.github.com>
2026-07-17 14:09:41 +0800

    Update MOD recommendations

--- src/js/data/mod-recommendations.js
+++ src/js/data/mod-recommendations.js
@@ -3,7 +3,7 @@ export const MOD_RECOMMENDATION_META = {
   "sourceUrl": "https://mod.3dmgame.com/Palworld",
   "sourceName": "3DM MOD站",
   "cutoff": "2026-07-10T16:00:00.000Z",
-  "updatedAt": "2026-07-17T06:03:49.668Z",
+  "updatedAt": "2026-07-17T06:09:18.511Z",
   "count": 96
 };
 
